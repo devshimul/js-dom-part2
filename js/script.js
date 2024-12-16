f1e9cda637401ebb3436c3a0232fcf8c0f7e5a58
devshimul <shimul.bisd@gmail.com>
2024-12-16 23:12:25 +0600

    js dom part2 start

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const heading = document.getElementsByTagName("h1");
+const heading2 = document.getElementsByTagName("h2");
+const liCollection = document.getElementsByTagName("li");
+
+const description = document.getElementsByClassName("description");
+console.log(description);
